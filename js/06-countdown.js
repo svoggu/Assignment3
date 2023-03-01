5bbea3a5ce6d764376d4309bb09f1d4f7867157e
svoggu <sirishavoggu@yahoo.co.in>
2023-02-28 20:45:13 -0800

    cleanup

--- js/06-countdown.js
+++ js/06-countdown.js
@@ -1,7 +1,3 @@
-// function count() {
-//     for (var cc=20;cc > 0;cc--) {
-//         document.getElementById("cco").innerHTML=cc;
-//     }
-// }
+
 let n = prompt("please enter a number!!");
 for (var x=n;x >= 0;x--) console.log(x);
